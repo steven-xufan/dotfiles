50de30aebe992bee14fe4ecc1ae75ceecf024991
Steven Xu Fan <67865954+steven-xufan@users.noreply.github.com>
2022-03-09 18:19:42 +0800

    Remove AstroVim and Clean out all plugins to start fresh again
    Turns out AstroVim is still quite buggy (lazygit is not working reliably yet)

--- plugins.lua
+++ plugins.lua
@@ -8,33 +8,6 @@ return require('packer').startup(function(use)
   -- My plugins here
   use 'wbthomason/packer.nvim'
 
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
-    config = function() require'nvim-tree'.setup {
-      highlight_focused_file = true,
-      update_focused_file = {
-          enable      = true,
-          update_cwd  = false,
-          ignore_list = {}
-      }
-    } end
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
   -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
