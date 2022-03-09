945289b98ed73da0608b623763aa9a954342c571
Steven Xu Fan <67865954+steven-xufan@users.noreply.github.com>
2022-03-09 18:21:07 +0800

    Add `astrovim` and `lscd` aliases
    astrovim to run a copy of astrovim for reference purpose.
    (complete setup is documented in Notion)

--- alias.sh
+++ alias.sh
@@ -1,3 +1,6 @@
 alias v=nvim
 alias v_init="v ~/.config/nvim/init.vim"
 alias v_plugin="v ~/.config/nvim/lua/plugins.lua"
+alias astrovim="XDG_CONFIG_HOME=~/.config/astro_nvim XDG_DATA_HOME=~/.config/astro_nvim nvim"
+
+alias lscd="ls | fzf | cd"
