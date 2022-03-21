df56f2699af2200f11458cb08c9902f1ffd6b3cb
Steven Xu Fan <67865954+steven-xufan@users.noreply.github.com>
2022-03-21 17:32:35 +0100

    Add function to clone dh repo

--- alias.sh
+++ alias.sh
@@ -4,3 +4,6 @@ alias v_plugin="v ~/.config/nvim/lua/plugins.lua"
 alias astrovim="XDG_CONFIG_HOME=~/.config/astro_nvim XDG_DATA_HOME=~/.config/astro_nvim nvim"
 
 alias lscd="ls | fzf | cd"
+
+
+function clone_dh () gh repo clone deliveryhero/$1 $2
