260a777a02b26e8a632de92ad509555f13e65937
Steven Xu Fan <67865954+steven-xufan@users.noreply.github.com>
2022-04-02 23:27:09 +0800

    Update neovim plugins.lua
    - Have packer use a popup window (instead of a new buffer)
    - Auto reload neovim whenever `plugins.lua` is saved
    (based on https://github.com/LunarVim/Neovim-from-scratch/)

--- alias.sh
+++ alias.sh
@@ -1,6 +1,5 @@
 alias v=nvim
-alias v_init="v ~/.config/nvim/init.vim"
-alias v_plugin="v ~/.config/nvim/lua/plugins.lua"
+alias v_plugin="v ~/.config/nvim/lua/user/plugins.lua"
 alias astrovim="XDG_CONFIG_HOME=~/.config/astro_nvim XDG_DATA_HOME=~/.config/astro_nvim nvim"
 
 alias lscd="ls | fzf | cd"
